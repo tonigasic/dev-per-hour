66c269274ce02f549ca6f2c0f42f04275d0d42eb
tonigasic <togasic@gmail.com>
2021-02-13 16:04:38 +0100

    delete axios request

--- src/Request.js
+++ src/Request.js
@@ -15,6 +15,21 @@ export const postRequest = (path, body, resolve, reject) => {
         })
 };
 
+export const deleteRequest = (path, resolve, reject) => {
+    axios.delete(path, {})
+        .then((response) => {
+            resolve(response);
+        })
+        .catch((err) => {
+            if (err.response.data && typeof err.response.data === "string") {
+                reject(err.response.data);
+            }
+            else {
+                reject('Internal Server Error');
+            }
+        })
+};
+
 export const putRequest = (path, body, resolve, reject) => {
     axios.put(path, body)
         .then((response) => {
