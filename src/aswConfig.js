77402abd585a26adcf5939fda72497c854584052
tonigasic <33573646+tonigasic@users.noreply.github.com>
2021-06-25 13:37:07 +0200

    Delete aswConfig.js

--- src/aswConfig.js
+++
@@ -1,7 +0,0 @@
-export const awsConfig = {
-    bucketName: 'dev-per-hour',
-    albumName: 'photos',
-    region: 'eu-central-1',
-    accessKeyId: 'AKIAJXCDJ3EDJFFPM6VA',
-    secretAccessKey: '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'
-};
